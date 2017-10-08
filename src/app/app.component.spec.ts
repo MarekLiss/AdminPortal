87ca68ea3d00774d9f509be1310673a5ae5b1d96
MaroM <maro.muszynski@gmail.com>
2017-10-08 21:51:53 +0200

    try to fix css not working on login screen

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';<router-outlet></router-outlet>
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
