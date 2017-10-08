2e07132d858a74750a3b96b88f92f6fe7e2fe526
MaroM <maro.muszynski@gmail.com>
2017-10-08 18:01:46 +0200

    add login service and clean login and navbar component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loggedIn: boolean;
+  username: string;
+  password: string;
 
-  ngOnInit() {
+	constructor (private loginService: LoginService) {
+    if(localStorage.getItem('PortalAdminHasLoggedIn') == '' || localStorage.getItem('PortalAdminHasLoggedIn') == null) {
+      this.loggedIn = false;
+    } else {
+      this.loggedIn = true;
+    }
   }
+
+  onSubmit() {
+  	this.loginService.sendCredential(this.username, this.password).subscribe(
+      res => {
+        this.loggedIn=true;
+        localStorage.setItem('PortalAdminHasLoggedIn', 'true');
+        location.reload();
+      },
+      err => console.log(err)
+    );
+  }
+
+  ngOnInit() {}
 
 }
