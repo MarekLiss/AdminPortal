7246efea8abaacebfd2e1e9bd965edaaf1424147
MarekLiss <marek.muszynski@liss.co.uk>
2018-02-16 20:19:35 +0000

    add char.js library and add a simple chart to show amount deposited on a time line

--- src/app/primary-transaction/primary-transaction.component.ts
+++ src/app/primary-transaction/primary-transaction.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {UserService} from '../user.service';
+import { UserService } from '../user.service';
 import { ActivatedRoute, Params } from '@angular/router';
+import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-primary-transaction',
@@ -9,9 +10,13 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 
 export class PrimaryTransactionComponent implements OnInit {
-
+  
+  
+  depositDates = [];
   username: string;
   primaryTransactionList: Object[];
+  amounts: Object[];
+  chart = [];
 
   constructor(private route: ActivatedRoute, private userService: UserService) {
     this.route.params.forEach((params: Params) => {
@@ -26,11 +31,43 @@ export class PrimaryTransactionComponent implements OnInit {
       res => {
         console.log(JSON.parse(JSON.stringify(res))._body);
         this.primaryTransactionList = JSON.parse(JSON.parse(JSON.stringify(res))._body);
+        this.amounts = JSON.parse(res['_body']).map(res => res.amount);
+        let dates = JSON.parse(res['_body']).map(res => res.date);
+        dates.forEach((date) => {this.depositDates.push(new Date(date).toLocaleTimeString('en', { year: 'numeric', month: 'short', day: 'numeric'}));
+            this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: this.depositDates,
+            datasets: [
+              {
+                data: this.amounts,
+                borderColor: '#3cba9f',
+                fill: false
+              }
+            ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [{
+                display: true
+              }],
+              yAxes: [{
+                display: true
+              }]
+            }
+          }
+        });
+        });
       },
       error => console.log(error)
     );
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+  }
 
 }
